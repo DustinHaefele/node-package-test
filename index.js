d4d458d0c91a33bf333879740100a17a31da1995
dustin22 <dustin@auditdeploy.com>
2020-10-07 09:08:26 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import {SphericalUtil, PolyUtil} from "node-geometry-library";
+
+
+
+const printMyName = name => {
+  let response = PolyUtil.isLocationOnPath(
+    {lat: 25.771, lng: -80.19}, // point object {lat, lng}
+    [
+      // poligon arrays of object {lat, lng}
+      {lat: 25.774, lng: -80.19},
+      {lat: 18.466, lng: -66.118},
+      {lat: 32.321, lng: -64.757}
+    ]
+  );
+  console.log(name, response)
+}
+
+module.exports = { printMyName }
